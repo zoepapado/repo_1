6d88feedb06a7b32d9399a6e58f69cd17e866cad
zoepapado <59338846+zoepapado@users.noreply.github.com>
2024-03-17 16:40:22 +0000

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+request name from the user
+store input into variable called 'name'
+print name
+request age from the user
+store input into variable called 'age'
+print age
+print Hello World!
+
+name = input('What is your first name?')
+print(name)
+age = input('What is your age in years?')
+print(age)
+
+print('Hello World!')
