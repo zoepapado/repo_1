c4b02216bbfa2d0b840cc038c82ab47ea6c7c000
zoepapado <59338846+zoepapado@users.noreply.github.com>
2024-03-17 17:10:43 +0000

    Update details.py

--- details.py
+++ details.py
@@ -1,11 +1,11 @@
-request name, age, house number and street name from the user
-store these into variables called 'name', 'age', 'house_number' and 'street_name' respectively
-print a sentence which incorporates these variables, i.e. 'This is [name]. He is [age] years old and lives at house number [house_number] on [street_name]'
-make sure to change all variable types to strings (if the input types were integers) where necessary
+#request name, age, house number and street name from the user
+#store these into variables called 'name', 'age', 'house_number' and 'street_name' respectively
+#print a sentence which incorporates these variables, i.e. 'This is [name]. He is [age] years old and lives at house number [house_number] on [street_name]'
+#make sure to change all variable types to strings (if the input types were integers) where necessary
 
 name = input('What is your full name?')
 age = input('How old are you (in years)?')
 house_number = input('What is your house number?')
 street_name = input('What is the name of your street?')
 
-print(f"This is {name}. He is {age} years old and lives at house number {house_number} on {street_address}.")
+print(f"This is {name}. He is {age} years old and lives at house number {house_number} on {street_name}.")
