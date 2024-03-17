959645d04c24ecbfb30f544a9a6d249eca819597
zoepapado <59338846+zoepapado@users.noreply.github.com>
2024-03-17 13:42:36 +0000

    file_cd.bat

---
+++ file_cd.bat
@@ -0,0 +1,17 @@
+
+#create three new folders in current directory
+mkdir folder_1
+mkdir folder_2
+mkdir folder_3
+
+#navigate to folder_1
+cd folder_1
+
+#create three new folders in folder_1
+mkdir folder_1_1
+mkdir folder_1_2
+mkdir folder_1_3
+
+#delete two folders in folder_1
+del folder_1_2
+del folder_1_3
